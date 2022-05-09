b29190ab6e52229447058e8d9753018c80586f41
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-09 07:51:40 +0200

    verification token complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,6 +33,26 @@ const register = async (req, res) => {
   });
 };
 
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Verification Failed');
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError('Verification Failed');
+  }
+
+  (user.isVerified = true), (user.verified = Date.now());
+  user.verificationToken = '';
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Email Verified' });
+};
+
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -69,4 +89,5 @@ module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 };
