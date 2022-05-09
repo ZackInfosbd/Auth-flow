07d04445b2ff110c055caeda499822ce84912180
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-09 10:11:37 +0200

    send verification email + verification url

---
+++ server/utils/nodemailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'gaylord.sporer33@ethereal.email',
+    pass: 'PwU1nqJpUnfhrc2R3B',
+  },
+};
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,24 +1,17 @@
 const nodemailer = require('nodemailer');
+const nodemailerConfig = require('./nodemailerConfig');
 
-const sendEmail = async () => {
+const sendEmail = async ({ to, subject, html }) => {
   let testAccount = await nodemailer.createTestAccount();
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-      user: 'gaylord.sporer33@ethereal.email',
-      pass: 'PwU1nqJpUnfhrc2R3B',
-    },
-  });
+  const transporter = nodemailer.createTransport(nodemailerConfig);
 
-  let info = await transporter.sendMail({
+  return transporter.sendMail({
     from: '"Zakaria Kourdali 👻" <kourdali.zakaria@gmail.com.com>', // sender address
     // to: 'bar@example.com, baz@example.com', // list of receivers
-    to: 'gaylord.sporer33@ethereal.email', // list of receivers
-    subject: 'Email verification', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Testing email?</b>', // html body
+    to,
+    subject,
+    html,
   });
 };
 
